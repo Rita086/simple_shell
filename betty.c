00aad3411badf990a37cb6be829105a6128d062e
Rita086 <gathogoritawanjiru@gmail.com>
2023-12-13 04:02:10 -0800

    file betty.c

---
+++ betty.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - alphabet to be printed x10 times
+* Return: void
+*/
+void print_alphabet(void)
+{
+	char c;
+	int k = 0;
+
+	while (k <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	k++;
+	}
+}
